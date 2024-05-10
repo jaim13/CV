6131a118f048591d46e613a55df4ad676eb48c6a
jaim13 <jaimmartinez13@gmail.com>
2024-05-10 13:21:09 -0600

    Re fixed prev and next btn on projects carousel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,11 +8,11 @@ export default function Projects() {
       <h1>Projects</h1>
       <div
         id="carouselExampleInterval"
-        class="carousel slide"
+        className="carousel slide"
         data-bs-ride="carousel"
       >
-        <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="10000">
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="10000">
             <ProjectCard
               title="Gym Project"
               description={
@@ -57,7 +57,7 @@ export default function Projects() {
               imageUrl="https://images.unsplash.com/photo-1590487988256-9ed24133863e?q=10&w=856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             />
           </div>
-          <div class="carousel-item" data-bs-interval="10000">
+          <div className="carousel-item" data-bs-interval="10000">
             <ProjectCard
               title="Mongo CRUD"
               description={
@@ -88,11 +88,13 @@ export default function Projects() {
             />
           </div>
         </div>
-        <button className={`carousel-control-prev ${styles.customControl}`} type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+         {/* Botón "prev" */}
+        <button className={`carousel-control-prev ${styles['carousel-button']}`} type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Previous</span>
         </button>
-        <button className={`carousel-control-next ${styles.customControl}`} type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        {/* Botón "next" */}
+        <button className={`carousel-control-next ${styles['carousel-button']}`} type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
           <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Next</span>
         </button>
